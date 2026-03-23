c64d8cee89dbd05cf7882076069146f385fde402
MATSUURA Satoshi <ayaya@MacBook-Pro-M4-Max.local>
2026-03-23 20:55:18 +0900

    chore: bump version to v2.3.74

--- Sources/QuickTranscriber/Constants.swift
+++ Sources/QuickTranscriber/Constants.swift
@@ -42,7 +42,7 @@ public enum Constants {
     public enum Version {
         public static let major = 2
         public static let minor = 3
-        public static let patch = 73
+        public static let patch = 74
         public static let string = "\(major).\(minor).\(patch)"
         public static let versionString = "v\(string)"
     }
